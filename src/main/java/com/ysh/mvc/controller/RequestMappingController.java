c3c93eb70de76c50ed67782ba726a096d7f5b155
83425 <yushihai2020@163.com>
2021-11-29 22:22:19 +0800

    Initial commit

---
+++ src/main/java/com/ysh/mvc/controller/RequestMappingController.java
@@ -0,0 +1,37 @@
+package com.ysh.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+//@RequestMapping("/hello")
+public class RequestMappingController {
+
+    @RequestMapping(
+            value={"/testRequestMapping","/test"},
+            method = {RequestMethod.GET,RequestMethod.POST},
+            params = {"username",}
+    )
+    public String success(){
+        return "success";
+    }
+
+    @GetMapping("/testGetMapping")
+    public String testGetMapping(){
+        return "success";
+    }
+
+    @RequestMapping(value = "/testPut",method = RequestMethod.PUT)
+    public String testPut(){
+        return "success";
+    }
+
+    @RequestMapping(value = "/testParamsAndHeaders",params = {"username","pwd"},
+            headers = {"Host=localhost:8080"}
+    )
+    public String testParamsAndHeaders(){
+        return "success";
+    }
+}
